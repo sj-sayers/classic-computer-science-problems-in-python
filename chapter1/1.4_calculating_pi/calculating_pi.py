0aba3f9bb2363b101e5903b1cb4cc2e01e69c8e4
Steven Sayers <ssayers@onica.com>
2020-11-09 17:24:43 -0500

    chapter1 complete

--- chapter1/1.4_calculating_pi/calculating_pi.py
+++ chapter1/1.4_calculating_pi/calculating_pi.py
@@ -6,7 +6,7 @@ def calculate_pi(n_terms: int) -> float:
     for _ in range(n_terms):
         pi += operation * (numerator / denominator)
         denominator += 2.0
-        operation *= -1.0
+        operation *= -1.0 # switch from addition to subtraction each iteration
     return pi
 
 if __name__ == "__main__": 
