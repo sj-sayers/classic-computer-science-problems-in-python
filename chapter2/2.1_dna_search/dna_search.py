e7c8081cee6513f834a8ea854d71277a558a439f
Steven Sayers <ssayers@onica.com>
2020-11-14 16:13:25 -0500

    linear search working. binary to come

---
+++ chapter2/2.1_dna_search/dna_search.py
@@ -0,0 +1,33 @@
+from enum import IntEnum
+from typing import Tuple, List
+
+Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]  # type alias for codons
+Gene = List[Codon]  # type alias for genes
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGGTTTATATACCCTAGAGGACTCCCTTT"
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s):  # don't run off end! make sure there is 3 Nucleotides available to form into codon
+            return gene
+        # initialize codon out of three nucleotides
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]]) # build codon based on current spot in for loop range
+        gene.append(codon)  # add codon to gene
+    return gene
+
+my_gene: Gene = string_to_gene(gene_str)
+
+def linear_contains(gene: Gene, key_codon: Codon) -> bool:
+    for codon in gene:
+        if codon == key_codon:
+            return True
+        return False
+
+
+acg: Codon = (Nucleotide.A, Nucleotide.C, Nucleotide.G) # gene_str starts with acg
+gat: Codon = (Nucleotide.G, Nucleotide.A, Nucleotide.T) # but does not include a gat codon 
+print(linear_contains(my_gene, acg)) # True
+print(linear_contains(my_gene, gat)) # False
